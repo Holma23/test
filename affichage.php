4896c76f3a0103832cf2d19823a08847d2b8d06b
holma23 <ahlem.benabderrazak@esprit.tn>
2018-11-05 11:42:52 +0100

    first commit

---
+++ affichage.php
@@ -0,0 +1,8 @@
+<?php
+$nom="Sam";
+$anneeNaissance = 2000;
+$d=date('Y');
+$age=$d-$anneeNaissance;
+echo 'Bonjour Mr '.$nom.' vous avez '.$age.' ans'.'<br>';
+echo "Bonjour Mr {$nom} vous avez {$age} ans<br>"
+?>
